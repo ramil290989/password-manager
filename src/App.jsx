25162e16c1bf866436d2204e279a1a8e922ab87d
ramil290989 <=>
2023-11-11 12:21:44 +0500

    rename and add Routes

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AuthContext from './context/AuthContext.jsx';
+import Login from './pages/LogIn.jsx';
+import SignUp from './pages/SignUp.jsx';
+import Main from './pages/Main.jsx';
+import PageNotFound from './pages/PageNotFound.jsx';
+
+const App = () => {
+  const username = localStorage.getItem('pasManUsername');
+  const token = localStorage.getItem('pasManToken');
+  const { authData, setAthData } = useState({ username, token });
+  return (
+    <AuthContext.Provider value={{ authData, setAthData }}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='*' element={<PageNotFound />}/>
+          <Route path='login' element={<Login />} />
+          <Route path='signup' element={<SignUp />} />
+          <Route path='/' element={<Main />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthContext.Provider>
+  );
+}
+
+export default App;
