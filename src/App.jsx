a9c98653e965a5f72a274b8f93dd25eab7d4921a
ramil290989 <=>
2023-11-11 14:18:40 +0500

    add css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import AuthContext from './context/AuthContext.jsx';
 import PrivateRoute from './components/PrivateRoute.jsx';
 import Login from './pages/LogIn.jsx';
