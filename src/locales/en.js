97a1b4060b5e88c5c99a8c2bd68bcfec62bcdff8
ramil290989 <=>
2023-11-14 18:02:38 +0500

    add i18n

---
+++ src/locales/en.js
@@ -0,0 +1,12 @@
+const en = {
+  translation: {
+    errors: {
+      401: 'Incorrect username or password',
+      403: 'The token is invalid',
+      409: 'This username already exists',
+      431: 'Connection error',
+    }
+  }
+};
+
+export default en;
