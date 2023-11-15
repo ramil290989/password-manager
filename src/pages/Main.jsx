c9a763331b51469a585b8d193e0ea3735fa2633c
ramil290989 <=>
2023-11-15 18:15:16 +0500

    add state funcs

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,12 +1,37 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
+import React, { useContext, useEffect } from 'react';
+import { Container, Row, Col, Spinner } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import AuthContext from '../context/AuthContext.jsx';
+import { getPasswords, selectors } from '../slices/passwordsSlice.js';
 
 const Main = () => {
-  return (
-    <Container>
-      eeee
-    </Container>
-  );
+  const dispatch = useDispatch();
+  const loadingStatus = useSelector((state) => state.passwords.loadingStatus);
+  const { authData } = useContext(AuthContext);
+  const { token } = authData;
+  const passwords = useSelector(selectors.selectAll);
+
+  useEffect(() => {
+    if (loadingStatus === 'loading') {
+      dispatch(getPasswords(token));
+    }
+  }, [token, dispatch, loadingStatus]);
+
+  switch (loadingStatus) {
+    case 'loading':
+      return (
+        <Container className='text-center'>
+          <Spinner animation='border' variant='primary' className='m-5' />
+        </Container>
+      );
+    case 'idle':
+      console.log(passwords);
+      return (
+        null
+      );
+    default:
+      return null;
+  };
 };
 
 export default Main;
