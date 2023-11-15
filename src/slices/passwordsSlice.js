1cfb9b2ecaacba2416c6ae7e010a69ced0ddc02c
ramil290989 <=>
2023-11-15 18:14:42 +0500

    add async thunk

--- src/slices/passwordsSlice.js
+++ src/slices/passwordsSlice.js
@@ -1,4 +1,16 @@
-import { createEntityAdapter, createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk ,createEntityAdapter, createSlice } from '@reduxjs/toolkit';
+import apiRoutes from '../apiRoutes.js';
+import axios from 'axios';
+
+export const getPasswords = createAsyncThunk(
+  'loadPasswords',
+  async (token) => {
+    const authHeader = { headers: { Authorization: token } };
+    const path = apiRoutes.data();
+    const response = await axios.get(path, authHeader);
+    return response.data;
+  }
+);
 
 const passwordsAdapter = createEntityAdapter();
 
@@ -10,11 +22,23 @@ const initialState = passwordsAdapter.getInitialState({
 const passwordSlice = createSlice({
   name: 'passwords',
   initialState,
-  reducers: {
-    addPassword: passwordsAdapter.addOne,
-    changePassword: passwordsAdapter.upsertOne,
-    removePassword: passwordsAdapter.removeOne,
-  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getPasswords.pending, (state) => {
+        state.loadingStatus = 'loading';
+        state.error = null;
+      })
+      .addCase(getPasswords.fulfilled, (state, action) => {
+        passwordsAdapter.addMany(state, action.payload);
+        state.loadingStatus = 'idle';
+        state.error = null;
+      })
+      .addCase(getPasswords.rejected, (state, action) => {
+        state.loadingStatus = 'failed';
+        state.error = action.payload.error;
+      })
+  }
 });
 
 export const selectors = passwordsAdapter.getSelectors((state) => state.passwords);
