40e30b52163073ea3bf81deb16974f2bb39aa52d
ramil290989 <=>
2023-11-17 19:17:53 +0500

    fix navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,34 @@
-import React from 'react';
-import { Navbar, Container } from 'react-bootstrap';
-import NavbarUserInfo from './NavbarUserInfo.jsx';
+import React, { useContext } from 'react';
+import { Navbar, Container, Nav, NavDropdown, Button } from 'react-bootstrap';
+import AuthContext from '../context/AuthContext.jsx';
 
 const HeaderNav = () => {
+  const { authData, setAuthData } = useContext(AuthContext);
+  const { username } = authData;
+
+  const logOut = () => {
+    localStorage.removeItem('pasManUsername');
+    localStorage.removeItem('pasManToken');
+    setAuthData({});
+  };
+
   return (
-    <Navbar>
+    <Navbar expand='md' className='bg-body-tertiary'>
       <Container>
-        <Navbar.Brand hrea='/'>Password Manager</Navbar.Brand>
-        <NavbarUserInfo />
+        <Navbar.Brand href='/'>Password Manager</Navbar.Brand>
+        <Navbar.Toggle aria-controls='navbar' />
+        <Navbar.Collapse id='navbar' className='justify-content-end'>
+          <Nav className='mx-md-3'>
+            <Nav.Link className='text-dark' href=''>add password</Nav.Link>
+            <Nav.Link className='text-dark' href=''>settings</Nav.Link>
+          </Nav>
+          <Navbar.Text className='my-md-3 mx-md-3'>
+            sign in as {username}
+          </Navbar.Text>
+          <div>
+            <Button onClick={logOut}>LogOut</Button>
+          </div>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
