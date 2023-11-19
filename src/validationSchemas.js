9d2cb73594c2b30f6b49191d10bba798fdffc3db
ramil290989 <garifullinramil290989@gmail.com>
2023-11-19 22:39:36 +0500

    add signUp validation

---
+++ src/validationSchemas.js
@@ -0,0 +1,16 @@
+import * as Yup from 'yup';
+
+const validationSchemaSignUp = (t) => Yup.object({
+  username: Yup.string()
+    .min(3, t('validations.min3max20'))
+    .max(20, t('validations.min3max20'))
+    .required(t('validations.required')),
+  password: Yup.string()
+    .min(6, t('validations.min6'))
+    .required(t('validations.required')),
+  confirmPassword: Yup.string()
+    .oneOf([Yup.ref('password'), null], t('validations.oneOf'))
+    .required(t('validations.required')),
+});
+
+export { validationSchemaSignUp };
