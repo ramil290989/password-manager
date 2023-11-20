e97ced15efbdb970d7a16753dd38aced79c1357a
ramil290989 <garifullinramil290989@gmail.com>
2023-11-21 02:30:13 +0500

    add modalsSlice

--- src/slices/index.js
+++ src/slices/index.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import passwordsReducer from './passwordsSlice.js';
+import modalsReducer from './modalsSlice.js';
 
 export default configureStore({
   reducer: {
     passwords: passwordsReducer,
+    modals: modalsReducer,
   },
 });
---
+++ src/slices/modalsSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  window: null,
+  id: null,
+};
+
+const modalsSlice = createSlice({
+  name: 'modals',
+  initialState,
+  reducers: {
+    modalShow: (state, { payload }) => {
+      state.window = payload.window;
+      state.id = payload.id;
+    },
+    modalHide: () => initialState
+  },
+});
+
+export const { actions } = modalsSlice;
+export default modalsSlice.reducer;
