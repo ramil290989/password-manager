40b8ec025f6e00802fb902990498d481cc786eb6
ramil290989 <garifullinramil290989@gmail.com>
2023-11-22 00:41:41 +0500

    fix loading page

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Container, Spinner } from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
 
-const Loading = () => (
-  <Container className='text-center'>
-    <Spinner animation='border' variant='primary' className='m-5' />
-  </Container>
-);
+const Loading = () => {
+  const { t } = useTranslation();
+
+  return (
+    <Container className='text-center'>
+      <Spinner animation='border' variant='primary mt-5' />
+      <span className='fs-2 text-primary'> {t('other.loading')}</span>
+      <p className='fs-4'>{t('other.loadingText')}</p>
+    </Container>
+  );
+};
 
 export default Loading;
