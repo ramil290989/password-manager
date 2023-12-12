90f646d9302fa0e315a6fe76cc6fa4bd0d61fad4
ramil290989 <garifullinramil290989@gmail.com>
2023-12-13 02:42:03 +0500

    add change language

--- src/components/navbar/NavbarControl.jsx
+++ src/components/navbar/NavbarControl.jsx
@@ -11,21 +11,27 @@ const NavbarControl = () => {
   const { username } = authData;
   const dispatch = useDispatch();
 
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
   const logOut = useLogOut();
 
   const openAddPasswordModal = () => {
     dispatch(modalsActions.modalShow({ window: 'addPassword' }));
   };
+  const openChangeUserPasswordModal = () => {
+    dispatch(modalsActions.modalShow({ window: 'changeUserPassword'}));
+  };
+  const changeLanguage = (lng) => i18n.changeLanguage(lng);
 
   return username
     ? (
         <>
           <Nav className='mx-md-2 mx-lg-4 mb-2 mb-md-0'>
             <Nav.Link className='text-dark' onClick={openAddPasswordModal}>{t('navbar.addPassword')}</Nav.Link>
-            <NavDropdown title='Настройки'>
-              <NavDropdown.Item>Язык</NavDropdown.Item>              
-              <NavDropdown.Item>Изменить пароль</NavDropdown.Item>
+            <NavDropdown title={t('navbar.settings')}>
+              <NavDropdown.Item onClick={() => changeLanguage('ru')}>Русский</NavDropdown.Item>
+              <NavDropdown.Item onClick={() => changeLanguage('en')}>English</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item onClick={openChangeUserPasswordModal}>{t('navbar.changeUserPassword')}</NavDropdown.Item>
             </NavDropdown>
           </Nav>
           <Navbar.Text className='mx-md-2'>
