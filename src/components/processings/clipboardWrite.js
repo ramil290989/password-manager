f1aa9bf05ceaf9c8058e9237bf6dd3b1d8a9a5ed
ramil290989 <garifullinramil290989@gmail.com>
2023-12-21 01:29:20 +0500

    add Ctrl+C

--- src/components/processings/clipboardWrite.js
+++ src/components/processings/clipboardWrite.js
@@ -1,7 +1,12 @@
-const clipboardWrite = (password, setShow) => {
-  navigator.clipboard.writeText(password);
-  setShow(true);
-  setTimeout(() => { setShow(false); }, 1000);
+const clipboardWrite = (id, setOverlayShow) => {
+  const range = document.createRange();
+  range.selectNode(document.querySelector(`#password-span-${id}`));
+  window.getSelection().removeAllRanges();
+  window.getSelection().addRange(range);
+  document.execCommand('copy');
+  window.getSelection().removeAllRanges();
+  setOverlayShow(true);
+  setTimeout(() => { setOverlayShow(false); }, 1000);
 };
 
 export default clipboardWrite;
